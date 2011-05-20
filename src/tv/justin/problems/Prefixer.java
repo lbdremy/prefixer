fda3889165db14a3bf8d4622fee2ab1a487c5913
lbdremy <lbdremy@lbdremy-Latitude-E6500>
2011-05-20 15:06:11 -0700

    Remove useless import

--- src/tv/justin/problems/Prefixer.java
+++ src/tv/justin/problems/Prefixer.java
@@ -6,8 +6,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Stack;
 
 public class Prefixer {
